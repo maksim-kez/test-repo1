ce4ec2e87a784626271866944e0686c2c35e07db
maksim-kez <maksim.kez@innowise.com>
2025-12-08 17:07:25 +0100

    added new tests

---
+++ test-repo1.E2E.Tests/SomeOtherTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace test_repo1.E2E.Tests;
+
+public class SomeOtherTests
+{
+    [Fact]
+    public void TestPass()
+    {
+        Assert.True(true);
+    }
+    
+    [Fact]
+    public void TestFail()
+    {
+        Assert.True(false);
+    }
+    
+    [Fact]
+    public void TestSkip()
+    {
+        Assert.True(true);
+    }
+}
---
+++ test-repo1.Tests/SomeOtherTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace test_repo1.Tests;
+
+public class SomeOtherTest
+{
+    [Fact]
+    public void TestPass()
+    {
+        Assert.True(true);
+    }
+    
+    [Fact]
+    public void TestFail()
+    {
+        Assert.True(false);
+    }
+    
+    [Fact]
+    public void TestSkip()
+    {
+        Assert.True(true);
+    }
+}
